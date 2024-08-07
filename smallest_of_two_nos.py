236085967f35d962b8747fb40a9f134b8ec35cbe
BhoomikaJM <bhoomikajm07@gmail.com>
2024-08-07 20:56:13 +0530

    Add files via upload

---
+++ smallest_of_two_nos.py
@@ -0,0 +1,6 @@
+firstNum = int(input("Enter first number: "))
+secondNum = int(input("Enter second number: "))
+if firstNum < secondNum:
+    print("The first number is smallest.")
+else:
+    print("The second number is smallest.")
